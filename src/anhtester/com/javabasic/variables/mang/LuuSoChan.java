7b2a941eb9f9ce0d59a3970a294a9d5c2cdc3427
manh1987 <dinhvanmanh1987@gmail.com>
2024-05-06 22:40:36 +0700

    manh

--- src/anhtester/com/javabasic/variables/mang/LuuSoChan.java
+++ src/anhtester/com/javabasic/variables/mang/LuuSoChan.java
@@ -5,7 +5,7 @@ public class LuuSoChan {
 
         //Mảng 1 để tạo số từ 1-50
         int mang1[] = new int[50];
-        int mang2[] = new int[50]
+        int mang2[] = new int[50] ;
         for (int i = 0;i < mang1.length;i++) {
             mang1[i] = i+1;
         }
@@ -13,7 +13,7 @@ public class LuuSoChan {
             System.out.println(mang1[i]);
             //Mảng 2 đe lưu những giá trị số chẵn
             if (mang1[i] % 2 == 0){
-                mang2[i] = mang1[i]
+                mang2[i] = mang1[i];
             }
         }
 
@@ -21,9 +21,9 @@ public class LuuSoChan {
             System.out.println(mang2[i]);
         }
 
-        String b[] = new  String[3];
-        b[0] = 'Selenium';
-        
+//        String b[] = new  String[3];
+//        b[0] = 'Selenium';
+//
 
     }
 }
